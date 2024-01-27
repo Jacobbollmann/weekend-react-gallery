02a910e38cbb678b7c9829f70e358ba23634440a
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-01-27 16:54:10 -0600

    Completed server side put route and tested with postman

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,7 +4,20 @@ const router = express.Router();
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  const updateItem = parseInt(req.params.id);
+  const addOne = +1;
+  console.log('Updating item:', updateItem);
+  const dbQuery = 'UPDATE "gallery" SET "likes" = $1 WHERE "id" = $2;';
+
+  pool
+    .query(dbQuery, [addOne, updateItem])
+    .then((result) => {
+      res.send('Item updated:').status(201);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
 // GET /gallery
